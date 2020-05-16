1389b53b59f22e79549fd115bd66dadbef10bcd7
Matine temory <temorymatine@gmail.com>
2020-05-16 19:07:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,14 +2,15 @@ def square_array(array)
 array = [1,2,3]
 arrayy=[9,10,16,25]
 new_array = []
+new_arrayy = []
  
 array.length.times do |index|
   new_array.push(array[index] ** 2)
 end
 new_array
 
-array.length.times do |index|
-  new_array.push(array[index] ** 2)
+arrayy.length.times do |index|
+  new_arrayy.push(arrayy[index] ** 2)
 end
-
+new_arrayy
 end

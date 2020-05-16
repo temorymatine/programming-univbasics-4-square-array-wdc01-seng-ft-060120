81094ceb7d23df8735335ab6be821baefb2c3a0a
Matine temory <temorymatine@gmail.com>
2020-05-16 18:47:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   counter=0 
+  new_array=[]
   while array[counter] do
     new_array(array[counter]**2) 
-    return new_array
     counter+=1
   end
+   return new_array
 end
